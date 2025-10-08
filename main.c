646c5f688d28075d1c374fe9cf23673d390d7a92
Mathilde <marco.mathilde@orange.fr>
2025-10-08 11:56:53 +0000

    étape 4: créer les conditions

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <stdkib.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 int main()
@@ -7,11 +7,36 @@ int main()
 	int nb2;
 	char signe;
 	printf("Bonjour utilisateur.rice ! :)\n");
-	printf("Saisir un opérateur parmi la liste: + - * / % :");
-        scanf("%c", signe);
+	printf("Saisir un opérateur parmi la liste: + - * / %% :");
+        scanf("%c", &signe);
 	printf("Saisir un nombre entier: ");
-	scanf("%d", nb1);
+	scanf("%d", &nb1);
 	printf("Saisir un deuxième nombre entier: ");
-	scanf("%d", nb2);
+	scanf("%d", &nb2);
+
+	if (signe == 43)
+	{
+		printf("Une addition ! \n");
+	}
+	else if (signe == 45)
+	{
+		printf("Une soustraction ! \n");
+	}
+	else if (signe == 42)
+	{
+		printf("Une multiplication ! \n");
+	}
+	else if (signe == 47)
+	{
+		printf("Une division ! \n");
+	}
+	else if (signe == '%')
+	{
+		printf("Un modulo ! \n");
+	}
+	else
+	{
+		printf("L'opérateur ne fait pas partie de la liste, désolé.\n");
+	}
 	exit(0);
 }
