04068394f92671113d3f1480406a4eba7e098fcd
Mathilde <marco.mathilde@orange.fr>
2025-10-08 09:58:29 +0000

    étape 3: récupérer les infos !

--- main.c
+++ main.c
@@ -6,11 +6,12 @@ int main()
 	int nb1;
 	int nb2;
 	char signe;
+	printf("Bonjour utilisateur.rice ! :)\n");
+	printf("Saisir un opérateur parmi la liste: + - * / % :");
+        scanf("%c", signe);
 	printf("Saisir un nombre entier: ");
 	scanf("%d", nb1);
 	printf("Saisir un deuxième nombre entier: ");
 	scanf("%d", nb2);
-	printf("Saisir un opérateur :");
-	scanf("%c", signe);
 	exit(0);
 }
