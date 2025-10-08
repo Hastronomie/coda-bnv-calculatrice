ffc44b6878d908f494ab5b485ee82c344f0b0f2b
Mathilde <marco.mathilde@orange.fr>
2025-10-08 09:49:15 +0000

    étape 1: ajout des variables

--- main.c
+++ main.c
@@ -3,5 +3,14 @@
 
 int main()
 {
-	exit(0)
+	int nb1;
+	int nb2;
+	char signe;
+	printf("Saisir un nombre entier: ");
+	scanf("%d", nb1);
+	printf("Saisir un deuxième nombre entier: ");
+	scanf("%d", nb2);
+	printf("Saisir un opérateur :");
+	scanf("%c", signe);
+	exit(0);
 }
